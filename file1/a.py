950c6ca56a837b8957f5f228edabfdda467f27bf
prakash patil <prakashpatilmeti@gmail.com>
2024-02-23 11:42:18 +0530

    added constructer and variables to Car class

--- file1/a.py
+++ file1/a.py
@@ -1,3 +1,10 @@
 
 class Car:
-    pass
+    
+    country = 'India'
+    
+    def __init__(self, car_name, car_color, engine, model) -> None:
+        self.car_name = car_name
+        self.car_color = car_color
+        self.engine = engine
+        self.model = model
